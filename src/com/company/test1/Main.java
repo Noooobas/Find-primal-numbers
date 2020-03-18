dc697ff00347dfe9044eda00ea5b8542f8a58ab3
Alex <prediator123@gmail.com>
2020-03-18 13:05:59 +0400

    - replaced inner cycle with single if statment to decrease iterations number

--- src/com/company/test1/Main.java
+++ src/com/company/test1/Main.java
@@ -4,6 +4,8 @@ import java.util.ArrayList;
 
 public class Main {
 
+    static int itercount;
+
     public static void main(String[] args) {
 
         System.out.println("Hello! Primal numbers are: ");
@@ -13,21 +15,16 @@ public class Main {
     // Checks every odd number starting from 3
     private static void getPrimalNumbers(){
 
-        ArrayList<Integer> primalNumbers= new ArrayList<>();
-        primalNumbers.add(1);
+        ArrayList<Integer> primalNumbers = new ArrayList<>();
         primalNumbers.add(2);
         boolean isPrimal;
+        itercount = 0;
 
         // Skips every even number, because they always can be divided by 2
         for (int i =3; i <= 100; i+=2){
             isPrimal = true;
-            for (int j =3; j <i; j+=2){
-                if (i%j == 0) {
-                    // if not primal, stop further check and mark as not primal
-                    isPrimal = false;
-                    break;
-                }
-            }
+            itercount++;
+            if (i>7 && (i % 3 == 0 || i % 5 == 0 || i % 7 == 0)) isPrimal = false;
             // Will fail if number is not primal
             if (isPrimal) {
                 // Add current number as next element of the array list
@@ -40,9 +37,9 @@ public class Main {
 
     // Prints values of array list and their count
     private static void showPrimalNumbers(ArrayList primalNumbers){
-
         System.out.println(primalNumbers);
         System.out.println(primalNumbers.size() + " numbers in total");
+        System.out.println("Amount of iterations: "+ itercount);
 
     }
 
